7f05a1d6819703f30f864f5b7e96ccafa8263401
slim <saleemadat@gmail.com>
2024-03-29 12:12:52 +0000

    bencode parser

--- main.cpp
+++ main.cpp
@@ -1,12 +1,64 @@
+
 #include <fmt/core.h>
 
-#include "bencode_parser.h"
+#include <fstream>
+#include <string>
+
+#include "bencode_parser.hpp"
+
+struct TorrentFile {
+  std::string announce;
+  std::string name;
+  int64_t piece_length;
+  std::string pieces;
+  int64_t length;
+  int64_t created_date;
+};
 
 int main() {
-  BencodeParser parser;
-  int result = parser.parse("d3:cow3:moo4:spam4:eggse", 0);
-  fmt::print("Result: {}\n", result);
+  // Open the torrent file
+  std::ifstream file("debian.torrent", std::ios::binary);
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file." << std::endl;
+    return 1;
+  }
 
-  return 5;
+  auto data = bencode::decode(file);
+
+  auto announe = data["announce"];
+  auto announe_value = std::get<bencode::string>(announe);
+
+  auto comment = data["comment"];
+  auto comment_value = std::get<bencode::string>(comment);
+
+  auto created_by = data["created by"];
+  auto created_by_value = std::get<bencode::string>(created_by);
+
+  auto created_date = data["creation date"];
+  auto created_date_value = std::get<bencode::integer>(created_date);
+
+  auto length = data["info"]["length"];
+  auto length_value = std::get<bencode::integer>(length);
+
+  auto name = data["info"]["name"];
+  auto name_value = std::get<bencode::string>(name);
+
+  auto piece_length = data["info"]["piece length"];
+  auto piece_length_value = std::get<bencode::integer>(piece_length);
+
+  auto pieces = data["info"]["pieces"];
+  auto pieces_value = std::get<bencode::string>(pieces);
+
+  TorrentFile torrent_file;
+  torrent_file.announce = announe_value;
+  torrent_file.name = name_value;
+  torrent_file.piece_length = piece_length_value;
+  torrent_file.pieces = pieces_value;
+  torrent_file.length = length_value;
+  torrent_file.created_date = created_date_value;
+
+  fmt::print("announce: {}\n", torrent_file.announce);
+
+  return 0;
 }
 

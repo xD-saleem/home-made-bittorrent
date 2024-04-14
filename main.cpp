14b857e3a5d31a422a80b09e32d5a41c9e097507
slim <saleemadat@gmail.com>
2024-04-14 17:36:36 +0100

    added cmake

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 
 #include "torrent_parser.h"
 
-struct Dependency {};
-
 int main() {
   std::string torrentFilePath = "debian.torrent";
   fmt::print("Reading torrent file: {}\n", torrentFilePath);
 
   torrent_parser parser(torrentFilePath);
 
-  // parser.getFileName();
+  int a = parser.getFileSize();
+  fmt::print("File size: {}\n", a);
 };
 

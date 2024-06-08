ad59f3cd7afeaf373241334651a215543fe7c29b
slim <saleemadat@gmail.com>
2024-06-08 14:35:26 +0100

    added simple test

---
+++ include/TorrentFileParser_test.cpp
@@ -0,0 +1,8 @@
+#include "TorrentFileParser.h"
+
+#include <gtest/gtest.h>
+
+TEST(PieceManagerTest, GetFileName) {
+  // TorrentFileParser tfp = TorrentFileParser("test.torrent");
+  EXPECT_EQ("test", "test");
+}

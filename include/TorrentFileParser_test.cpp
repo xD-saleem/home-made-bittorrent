a44284504760ffe1544195866f32d883a01fab48
slim <saleemadat@gmail.com>
2024-06-12 22:06:50 +0100

    added more tests

--- include/TorrentFileParser_test.cpp
+++ include/TorrentFileParser_test.cpp
@@ -3,6 +3,26 @@
 #include <gtest/gtest.h>
 
 TEST(PieceManagerTest, GetFileName) {
-  // TorrentFileParser tfp = TorrentFileParser("test.torrent");
-  EXPECT_EQ("test", "test");
+  TorrentFileParser tfp = TorrentFileParser("debian.torrent");
+  EXPECT_EQ(tfp.getFileName(), "debian-12.5.0-amd64-netinst.iso");
 }
+
+TEST(PieceManagerTest, GetFileSize) {
+  TorrentFileParser tfp = TorrentFileParser("debian.torrent");
+  EXPECT_EQ(tfp.getFileSize(), 659554304);
+}
+
+TEST(PieceManagerTest, GetAnnounce) {
+  TorrentFileParser tfp = TorrentFileParser("debian.torrent");
+  EXPECT_EQ(tfp.getAnnounce(), "http://bttracker.debian.org:6969/announce");
+}
+
+TEST(PieceManagerTest, getInfoHash) {
+  TorrentFileParser tfp = TorrentFileParser("debian.torrent");
+  EXPECT_EQ(tfp.getInfoHash(), "2b66980093bc11806fab50cb3cb41835b95a0362");
+}
+
+// TEST(PieceManagerTest, getPieceLength) {
+//   TorrentFileParser tfp = TorrentFileParser("debian.torrent");
+//   EXPECT_EQ(tfp.splitPieceHashes(), "");
+// }

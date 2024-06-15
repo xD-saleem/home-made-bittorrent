3aaed27524a00b74f3ee7d5140d73d201b7f68ff
slim <saleemadat@gmail.com>
2024-06-15 16:46:52 +0100

    add tests

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 
 
+#include <expected>
 #include <iostream>
 #include <loguru/loguru.hpp>
 
 #include "TorrentClient.h"
+
+struct Error {
+  std::string message;
+};
+
+// Define a function that returns an expected<int, Error>
+std::expected<int, Error> divide(int a, int b) {
+  if (b == 0) {
+    return std::unexpected(Error{"Division by zero"});
+  }
+  return a / b;
+}
 
 int main(int argc, char* argv[]) {
   if (__cplusplus == 202101L)
@@ -22,23 +35,32 @@ int main(int argc, char* argv[]) {
     std::cout << "pre-standard C++." << __cplusplus;
   std::cout << "\n";
 
-  std::string torrentFilePath = "debian.torrent";
-  loguru::init(argc, argv);
+  auto result = divide(10, 2);
 
-  LOG_F(INFO, "Starting torrent client");
+  if (result) {
+    std::cout << "Result: " << *result << std::endl;  // Access the value
+  } else {
+    std::cout << "Error: " << result.error().message
+              << std::endl;  // Access the error
+  }
 
-  std::string filename = "debian.torrent";
-  std::string downloadDirectory = "./";
-  std::string downloadPath = downloadDirectory + filename;
-  std::string peerID = "peer_id";
+  // std::string torrentFilePath = "debian.torrent";
+  // loguru::init(argc, argv);
 
-  TorrentClient torrentClient(20, true, "./");
-
-  LOG_F(INFO, "Downloading torrent file");
-
-  torrentClient.downloadFile(downloadPath, downloadDirectory);
-
-  LOG_F(INFO, "Downloaded torrent file successfully");
+  // LOG_F(INFO, "Starting torrent client");
+  //
+  // std::string filename = "debian.torrent";
+  // std::string downloadDirectory = "./";
+  // std::string downloadPath = downloadDirectory + filename;
+  // std::string peerID = "peer_id";
+  //
+  // TorrentClient torrentClient(20, true, "./");
+  //
+  // LOG_F(INFO, "Downloading torrent file");
+  //
+  // torrentClient.downloadFile(downloadPath, downloadDirectory);
+  //
+  // LOG_F(INFO, "Downloaded torrent file successfully");
   return 0;
 };
 

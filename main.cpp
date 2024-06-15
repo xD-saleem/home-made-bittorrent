3f87d15b8ff770c967d10e2b8f5023978c88ca0f
slim <saleemadat@gmail.com>
2024-06-15 17:39:41 +0100

    added tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 
 
-#include <expected>
+// #include <expected>
 #include <iostream>
 #include <loguru/loguru.hpp>
 
@@ -10,13 +10,13 @@ struct Error {
   std::string message;
 };
 
-// Define a function that returns an expected<int, Error>
-std::expected<int, Error> divide(int a, int b) {
-  if (b == 0) {
-    return std::unexpected(Error{"Division by zero"});
-  }
-  return a / b;
-}
+// // Define a function that returns an expected<int, Error>
+// std::expected<int, Error> divide(int a, int b) {
+//   if (b == 0) {
+//     return std::unexpected(Error{"Division by zero"});
+//   }
+//   return a / b;
+// }
 
 int main(int argc, char* argv[]) {
   if (__cplusplus == 202101L)
@@ -35,32 +35,32 @@ int main(int argc, char* argv[]) {
     std::cout << "pre-standard C++." << __cplusplus;
   std::cout << "\n";
 
-  auto result = divide(10, 2);
+  // auto result = divide(10, 2);
 
-  if (result) {
-    std::cout << "Result: " << *result << std::endl;  // Access the value
-  } else {
-    std::cout << "Error: " << result.error().message
-              << std::endl;  // Access the error
-  }
+  // if (result) {
+  //   std::cout << "Result: " << *result << std::endl;  // Access the value
+  // } else {
+  //   std::cout << "Error: " << result.error().message
+  //             << std::endl;  // Access the error
+  // }
 
-  // std::string torrentFilePath = "debian.torrent";
-  // loguru::init(argc, argv);
+  std::string torrentFilePath = "debian.torrent";
+  loguru::init(argc, argv);
 
-  // LOG_F(INFO, "Starting torrent client");
-  //
-  // std::string filename = "debian.torrent";
-  // std::string downloadDirectory = "./";
-  // std::string downloadPath = downloadDirectory + filename;
-  // std::string peerID = "peer_id";
-  //
-  // TorrentClient torrentClient(20, true, "./");
-  //
-  // LOG_F(INFO, "Downloading torrent file");
-  //
-  // torrentClient.downloadFile(downloadPath, downloadDirectory);
-  //
-  // LOG_F(INFO, "Downloaded torrent file successfully");
+  LOG_F(INFO, "Starting torrent client");
+
+  std::string filename = "debian.torrent";
+  std::string downloadDirectory = "./";
+  std::string downloadPath = downloadDirectory + filename;
+  std::string peerID = "peer_id";
+
+  TorrentClient torrentClient(20, true, "./");
+
+  LOG_F(INFO, "Downloading torrent file");
+
+  torrentClient.downloadFile(downloadPath, downloadDirectory);
+
+  LOG_F(INFO, "Downloaded torrent file successfully");
   return 0;
 };
 

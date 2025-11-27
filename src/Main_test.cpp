24e821fbcbe08a5d210ef735017526d1022726fb
slim <saleemadat@gmail.com>
2025-11-27 17:13:21 +0000

    lowercase main

--- src/Main_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-  // Initialize Google Test framework
-  ::testing::InitGoogleTest(&argc, argv);
-  // Run tests
-  return RUN_ALL_TESTS();
-}

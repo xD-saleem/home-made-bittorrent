e22bb6c30e2437b1b933e71aa50d66d85a63ef6a
slim <saleemadat@gmail.com>
2025-12-28 10:27:03 +0000

    shared queue rename

---
+++ src/Queue.h
@@ -0,0 +1,87 @@
+
+#ifndef BITTORRENTCLIENT_QUEUE_H
+#define BITTORRENTCLIENT_QUEUE_H
+
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+
+/**
+ * Implementation of a thread-safe Queue. Code from
+ * https://stackoverflow.com/questions/36762248/why-is-stdqueue-not-thread-safe
+ */
+template <typename T>
+class Queue {
+ public:
+  Queue();
+  ~Queue();
+
+  T front();
+  T pop_front();
+
+  void push_back(T item);
+  void clear();
+
+  int size();
+  bool is_empty();
+
+ private:
+  std::deque<T> queue_;
+  std::mutex mutex_;
+  std::condition_variable cond_;
+};
+
+template <typename T>
+Queue<T>::Queue() = default;
+
+template <typename T>
+Queue<T>::~Queue() = default;
+
+template <typename T>
+T Queue<T>::front() {
+  std::unique_lock<std::mutex> lock(mutex_);
+  cond_.wait(lock, [this] { return !queue_.empty(); });
+  return queue_.front();
+}
+
+template <typename T>
+T Queue<T>::pop_front() {
+  std::unique_lock<std::mutex> lock(mutex_);
+  cond_.wait(lock, [this] { return !queue_.empty(); });
+  T front = std::move(queue_.front());
+  queue_.pop_front();
+  return front;
+}
+
+template <typename T>
+void Queue<T>::push_back(const T item) {
+  {
+    std::unique_lock<std::mutex> lock(mutex_);
+    queue_.push_back(std::move(item));
+  }
+  cond_.notify_one();  // notify one waiting thread
+}
+
+template <typename T>
+int Queue<T>::size() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  return queue_.size();
+}
+
+template <typename T>
+bool Queue<T>::is_empty() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  return size() == 0;
+}
+
+/**
+ * Empties the queue
+ */
+template <typename T>
+void Queue<T>::clear() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  queue_.empty();
+  cond_.notify_one();
+}
+
+#endif  // BITTORRENTCLIENT_QUEUE_H

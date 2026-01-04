1dfa0d9139bbad1745c8a18c60f0de51863c7389
slim <saleemadat@gmail.com>
2026-01-04 12:03:59 +0000

    Removed comments

--- src/Queue.h
+++ src/Queue.h
@@ -38,36 +38,19 @@ class Queue {
 
 template <typename T>
 T Queue<T>::front() {
-  // Logger::log(
-  // fmt::format("[Queue::front] tid={} waiting for item", thread_id_str()));
-
   std::unique_lock<std::mutex> lock(mutex_);
   cond_.wait(lock, [this] { return !queue_.empty(); });
-
-  // Logger::log(fmt::format("[Queue::front] tid={} woke up, size={}",
-  //                         thread_id_str(), queue_.size()));
 
   return queue_.front();
 }
 
 template <typename T>
 T Queue<T>::pop_front() {
-  // Logger::log(fmt::format("[Queue::pop_front] tid={} waiting for item",
-  // thread_id_str()));
-
   std::unique_lock<std::mutex> lock(mutex_);
   cond_.wait(lock, [this] { return !queue_.empty(); });
 
-  // Logger::log(
-  // fmt::format("[Queue::pop_front] tid={} popping item, size_before={}",
-  // thread_id_str(), queue_.size()));
-
   T front = std::move(queue_.front());
   queue_.pop_front();
-
-  // Logger::log(
-  //     fmt::format("[Queue::pop_front] tid={} item popped, size_after={}",
-  //                 thread_id_str(), queue_.size()));
 
   return front;
 }
@@ -77,19 +60,8 @@ void Queue<T>::push_back(T item) {
   {
     std::lock_guard<std::mutex> lock(mutex_);
 
-    // Logger::log(
-    //     fmt::format("[Queue::push_back] tid={} pushing item, size_before={}",
-    //                 thread_id_str(), queue_.size()));
-
     queue_.push_back(std::move(item));
-
-    // Logger::log(
-    //     fmt::format("[Queue::push_back] tid={} pushed item, size_after={}",
-    //                 thread_id_str(), queue_.size()));
   }
-
-  // Logger::log(fmt::format("[Queue::push_back] tid={} notifying one waiter",
-  // thread_id_str()));
 
   cond_.notify_one();
 }
@@ -106,23 +78,12 @@ bool Queue<T>::is_empty() {
   return queue_.empty();  // avoid double locking via size()
 }
 
-/**
- * Empties the queue
- */
 template <typename T>
 void Queue<T>::clear() {
   std::lock_guard<std::mutex> lock(mutex_);
-
-  // Logger::log(
-  //     fmt::format("[Queue::clear] tid={} clearing queue, size_before={}",
-  //                 thread_id_str(), queue_.size()));
-
   std::deque<T> empty;
-  queue_.swap(empty);
 
-  // Logger::log(fmt::format("[Queue::clear] tid={} queue cleared, notifying
-  // all",
-  //                         thread_id_str()));
+  queue_.swap(empty);
 
   cond_.notify_all();
 }
